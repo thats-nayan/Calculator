8a7848b57aed0c53d2cb105e3b9a3c988d8db65d
Nayan Hanandy <84136203+thats-nayan@users.noreply.github.com>
2022-06-07 17:41:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ let buttons = document.querySelectorAll(".button");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
         let key = this.innerHTML;
-        let inp = document.querySelector("input");
+        let inp = document.querySelector(".form");
         if (key == "AC") {
             string = "";
-            inp.placeholder = "0";
+            inp.innerHTML = "0";
         }
         else if (key == "Back") {
             string = string.slice(0,-1);
-            inp.placeholder = string;
+            inp.innerHTML= string;
         }
         else if (key == "=") {
             try {
                 eval(string)
             } catch (err) {
                 if(err instanceof SyntaxError)
-                inp.placeholder = "Invalid Input";
+                inp.innerHTML = "Invalid Input";
             }
             finally{
                 let num = eval(string);
-                inp.placeholder = num;
+                inp.innerHTML = num;
                 string = num.toString();
             }
         }
         else if(key == "+/-")
         {
             string = string.concat("*-1");
-            inp.placeholder = string;
+            inp.innerHTML = string;
         }
         else {
             string = string.concat(key);
-            inp.placeholder = string;
+            inp.innerHTML = string;
         }
     });
 }
